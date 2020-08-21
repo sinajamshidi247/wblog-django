73c3ad304c8a9025c30be1b6a715c72ed0a1a1dd
sisli7 <sina.j372@gmail.com>
2020-08-21 14:39:14 +0430

    third change

--- accounts/authenticate.py
+++ accounts/authenticate.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.models import User
 
 class EmailAuthBackend:
-    def authenticate(self,request,username=None,password=None)
-    try:
-        user=User.objects.get(email=username)
-        if user.check_password(password):
+    def authenticate(self,request,username=None,password=None):
+        try:
+            user=User.objects.get(email=username)
+            if user.check_password(password):
             return user
         return None
 
